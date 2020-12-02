136e4f4ef0440bad6ed6c07973abdbea938e627c
Robin Bürkli <robin.buerkli@students.fhnw.ch>
2020-12-02 20:01:22 +0100

    write tests and fix median function

--- lib/Hgrade/ListFunctions.hs
+++ lib/Hgrade/ListFunctions.hs
@@ -9,19 +9,22 @@ colsToRows ([]:_) = []
 colsToRows xs = (map head xs) : colsToRows (map tail xs)
 
 median :: [Int] -> Double
+median [] = 0
 median xs
-        | length xs `mod` 2 == 0 = fromIntegral (xs!!m + xs!!(m-1)) / 2
-        | otherwise = fromIntegral (xs!!m)
+        | length sortedList `mod` 2 == 0 = fromIntegral (sortedList!!m + sortedList!!(m-1)) / 2
+        | otherwise = fromIntegral (sortedList!!m)
         where m = (length xs) `div` 2
+              sortedList = sort xs
 
 calculateMedians :: [[Int]] -> [Double]
 calculateMedians [] = []
-calculateMedians (x:xs) = (median (sort x)) : calculateMedians xs
+calculateMedians (x:xs) = (median x) : calculateMedians xs
 
 doubleToStringList :: [Double] -> [String]
 doubleToStringList xs = map (\d -> show d) xs
 
 histogram :: [Int] -> (Int, Int, Int)
+histogram []  = (0,0,0)
 histogram col = (countPerPoints 0 col, countPerPoints 1 col, countPerPoints 2 col)
 
 countPerPoints :: Int -> [Int] -> Int
--- test/HgradeTest.hs
+++ test/HgradeTest.hs
@@ -1,26 +1,51 @@
 module Main (main) where
 
-import HGrade.Example (unlines')
+import Hgrade.ListFunctions
+import Data.List
 import Test.Tasty
 import Test.Tasty.HUnit
-
-
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Unit Tests" [medianTests, histoTests, c2rTests]
 
-unitTests :: TestTree
-unitTests = testGroup "Unit tests"
-  [ testCase "unlines' empty list" $
-      unlines' [] @?= "",
+medianTests :: TestTree
+medianTests = testGroup "Median tests"
+  [ testCase "median empty list" $
+          median [] @?= 0,
 
-    testCase "unlines' singleton list" $
-      unlines' ["hello"] @?= "hello",
+    testCase "median even list" $
+          median [1,0,6,1] @?= 1,
 
-    testCase "unlines' larger list" $
-      unlines' ["hello","world"] @?= "hello\nworld"  
+    testCase "median odd list" $
+          median [1,0,3] @?= 1.0,
+
+    testCase "median list of zeros" $
+          median [0,0] @?= 0
   ]
 
+histoTests :: TestTree
+histoTests = testGroup "Histogram tests"
+  [ testCase "histogram empty list" $
+          histogram [] @?= (0,0,0),
+
+    testCase "histogram four elements" $
+          histogram [1,0,2,1] @?= (1,2,1),
+
+    testCase "histogram 6 elements" $
+          histogram [1,0,2,1,1,0] @?= (2,3,1)
+  ]
+
+c2rTests :: TestTree
+c2rTests = testGroup "ColsToRows tests"
+  [ testCase "ColsToRows empty list" $
+          null (colsToRows []) @?= True,
+
+    testCase "colsToRows basic gradigs" $
+          colsToRows [[0,2,2,2,2],[0,2,2,2,1],[2,2,2,2,1],[2,0,0,1,2]] @?= [[0,0,2,2],[2,2,2,0],[2,2,2,0],[2,2,2,1],[2,1,1,2]],
+
+    testCase "colsToRows compare with transpose result" $
+          colsToRows [[0,2,2,2,2],[0,2,2,2,1],[2,2,2,2,1],[2,0,0,1,2]] @?= transpose [[0,2,2,2,2],[0,2,2,2,1],[2,2,2,2,1],[2,0,0,1,2]]
+  ]
