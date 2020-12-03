e1db15aaffc6de8e08f8d45bfd0e54555070c13e
Robin Bürkli <robin.buerkli@students.fhnw.ch>
2020-12-03 15:02:27 +0100

    add comments for the html module

--- lib/Hgrade/HTMLBuilder.hs
+++ lib/Hgrade/HTMLBuilder.hs
@@ -8,9 +8,9 @@ module Hgrade.HTMLBuilder where
 
 import Hgrade.ListFunctions
 
-
-createPage :: String -> String
-createPage content = unlines [
+-- | wraps the passed string into a standard html, so that every page metadata is the same
+renderPage :: String -> String
+renderPage content = unlines [
   "<!doctype html>",
   "<html lang='en'>",
   "  <head>",
@@ -20,55 +20,63 @@ createPage content = unlines [
   "  <body>" ++ content ++ "</body>",
   "</html>"]
 
-
+-- | wraps the string list into list items
 ul :: [String] -> String
 ul [] = ""
 ul [s] = "<li>" ++ s ++ "</li>"
 ul (s:ss) = "<li>" ++ s ++ "</li>" ++ ul ss
 
+-- | creates an anchor element with the given parameters
 a :: String -> String -> String
 a href title = "<a href=\"" ++ href ++ "\">" ++ title ++ "</a>"
 
+-- | wraps the string list into table head elements
 th :: [String] -> String
 th [] = ""
 th [s] = "<th>" ++ s ++ "</th>"
 th (s:ss) = "<th>" ++ s ++ "</th>" ++ th ss
 
+-- | transforms the string list into table rows
 tr :: [String] -> String
 tr [] = ""
 tr [s] = "<tr>" ++ s ++ "</tr>"
 tr (s:ss) = "<tr>" ++ s ++ "</tr>" ++ tr ss
 
+-- | transforms a list of showable values into table columns
 td :: Show a => [a] -> String
 td[] = ""
 td [s] = "<td>" ++ (show s) ++ "</td>"
 td (s:ss) = "<td>" ++ (show s) ++ "</td>" ++ td ss
 
+-- | creates an input field with a corresponding label
 labeledInput :: String -> String
 labeledInput name = "<label for=\"" ++ name ++"\">" ++ name ++ ": </label><input type=\"text\" name=\"" ++ name ++ "\" />"
 
-criteriaInput :: String -> String
-criteriaInput name = "<label for=\"" ++ name ++"\">" ++ name ++ ": </label><input type=\"text\" name=\"criteria[" ++ name ++ "]\" />"
-
+-- | build rows with the grader's data
 buildGraderRows :: [String] -> [[Int]] -> String
 buildGraderRows [] _ = ""
 buildGraderRows (_:_) [] = ""
 buildGraderRows (x:xs) (g: gs) = tr [("<td>" ++ x ++ "</td>" ++ td g)] ++ buildGraderRows xs gs
 
+-- | build the table row for medians
 buildMedianRow :: [Double] -> String
 buildMedianRow xs = tr ["<td>Median</td>" ++ concatMap(\d -> td ([d])) xs]
 
+-- | build the table row for histograms
 buildHistogramRow :: Int -> [[Int]]  -> String
 buildHistogramRow n xs = tr ["<td>Histograms</td>" ++ concatMap(\d -> "<td>" ++ (buildHistogramTable n d) ++ "</td>") xs]
 
+-- | build a single histogram
 buildHistogramTable :: Int -> [Int] -> String
 buildHistogramTable _ [] = ""
 buildHistogramTable n xs = "<table class='histo'>" ++ (buildHistogramRows n (histogram xs)) ++ "</table>"
 
+-- | build a single row within the histogram table
 buildHistogramRows :: Int -> (Int, Int, Int) -> String
 buildHistogramRows 0 _ = ""
 buildHistogramRows n (first, second, third) = tr[(tdColor n first ++ tdColor n second ++ tdColor n third)] ++ (buildHistogramRows (n - 1) (first, second, third))
 
+-- | print a white or black table column by comparing the two input values
 tdColor :: Int -> Int -> String
 tdColor x y
         | x > y = "<td class='white'></td>"
